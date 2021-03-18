4c365a62dd43fe62fdbe3fbf705a1f25b8ad5ca0
Fredrick Square <wool19018@wainuiomatahigh.school.nz>
2021-03-18 22:59:06 +0000

    1.2.0.0 Added new list system

--- main.py
+++ main.py
@@ -6,19 +6,29 @@ shopping = "yes"
 slist = []
 
 #Asks the user for their name
-name = input("What is your name?")
+name = input("What is your name? ")
 
 #Shopping begins
 while shopping == "yes":
-  item = input("What would you like to buy?")
+  item = input("What would you like to buy? ")
   item = item.strip()
   item = item.capitalize()
 
   #Adds item to list
   slist.append(item)
 
+  #Clears display
+  import os
+  def clear(): os.system('clear') 
+  clear()
+
+  #Prints header
+  print("------Smiths Online Shop------")
+  print("Your Cart: {}".format(slist))
+
   #Asks user if they wish to keep shopping
-  shopping = input("Do you wish to keep shopping?")
+  print("What would you like to buy? {}".format(item))
+  shopping = input("Do you wish to keep shopping? ")
   shopping = shopping.strip()
   shopping = shopping.casefold()
 

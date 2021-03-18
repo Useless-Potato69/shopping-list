1627af25394ff4c0eb96c55d55cb32923065c779
xqn <wool19018@wainuiomatahigh.school.nz>
2021-03-18 22:43:18 +0000

    Commented

--- main.py
+++ main.py
@@ -8,20 +8,28 @@ slist = []
 #Asks the user for their name
 name = input("What is your name?")
 
-#Adds items to cart
+#Shopping begins
 while shopping == "yes":
   item = input("What would you like to buy?")
   item = item.strip()
   item = item.capitalize()
+
+  #Adds item to list
   slist.append(item)
   shopping = input("Do you wish to keep shopping?")
   shopping = shopping.strip()
   shopping = shopping.casefold()
+
+  #Clears display
   import os
   def clear(): os.system('clear') 
   clear()
+
+  #Prints header
   print("------Smiths Online Shop------")
   print("Your Cart: {}".format(slist))
+
+#Ends user interaction
 print("Thank you for shopping with us {}!".format(name))
 print("See you next time!")
 
